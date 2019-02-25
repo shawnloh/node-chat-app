0ce79b028d1f75fc730af8dad7378e99f54cb9fa
Shawn Loh <shawnloh1996@gmail.com>
2019-02-25 18:36:01 +0800

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.listen(PORT, () => console.log(`server is online at port ${PORT}`));
